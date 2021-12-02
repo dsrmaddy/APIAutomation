4dcd49ee26802aac5f519a24431b25000ac902bd
Divya <divya@gmail.com>
2021-12-02 14:38:11 +0530

    added @test method for multi user post

--- API-Automation/src/test/java/io/swagger/petstore/UserDetailsTest.java
+++ API-Automation/src/test/java/io/swagger/petstore/UserDetailsTest.java
@@ -1,6 +1,6 @@
 package io.swagger.petstore;
 
-import com.tngtech.java.junit.dataprovider.DataProvider;
+//import com.tngtech.java.junit.dataprovider.DataProvider;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,12 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
-//import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
-//import static java.util.concurrent.TimeUnit.SECONDS;
 //import static org.hamcrest.Matchers.hasKey;
 
 public class UserDetailsTest extends BaseTest {
-
 
     //Test for post and create Single User
     @Test
@@ -36,6 +33,7 @@ public class UserDetailsTest extends BaseTest {
     //Get user details
     @Test
     public void getUserDetails(){
+
         Response userResponse =
                 given().spec(singleUserCreationSpec).
                         pathParam("username","singleuser").log().all().
@@ -43,5 +41,32 @@ public class UserDetailsTest extends BaseTest {
                         statusCode(200).and().extract().response();
 
         System.out.println("Response received "+userResponse.asPrettyString());
+    }
+
+    //post method for multiple users array
+    @Test
+    public void postUserArrayDetails(){
+
+        List<UserReqPayload> userArray = new ArrayList<>();
+
+        UserReqPayload user1 = new UserReqPayload(101, "petuser1", "pet1", "user1", "petuser1@gmail.com", "petuser1", "1234567890", 1);
+        UserReqPayload user2 = new UserReqPayload(102, "petuser2", "pet2", "user2", "petuser2@gmail.com", "petuser2", "1234567890", 1);
+        UserReqPayload user3 = new UserReqPayload(103, "petuser3", "pet3", "user3", "petuser3@gmail.com", "petuser3", "1234567890", 0);
+
+        userArray.add(user1);
+        userArray.add(user2);
+        userArray.add(user3);
+
+        UserRespPayload postResponse;
+        postResponse = given()
+                .spec(userArrayCreationSpec)
+                .body(userArray).log().all()
+                .when()
+                .post()
+                .as(UserRespPayload.class);
+
+        postResponse.printResponse();
+        Assert.assertEquals(200,postResponse.getCode());
+        Assert.assertEquals("unknown", postResponse.getType());
     }
 }
